0a6a66096e85ccf4e9d5941eeaae9267956514be
Timotius Leonardo <lionato14@gmail.com>
2025-10-15 16:21:12 +0700

    Feat: Add package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "ReverseExtension",
+    platforms: [
+        .iOS("14.0"),
+    ],
+    products: [
+        .library(
+            name: "ReverseExtension",
+            targets: ["ReverseExtension"]
+        ),
+    ],
+    targets: [
+        // Your main library target
+        .target(
+            name: "ReverseExtension",
+            dependencies: [],
+            path: "ReverseExtension"
+        ),
+    ]
+)
