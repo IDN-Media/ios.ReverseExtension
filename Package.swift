f026d1e713960b3cd92245209af5fbb5c6f764a7
Timotius Leonardo <lionato14@gmail.com>
2025-10-15 16:56:25 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,31 +1,29 @@
 // swift-tools-version: 5.10
-
 import PackageDescription
 
 let package = Package(
     name: "ReverseExtension",
     platforms: [
-        .iOS("14.0"),
+        .iOS(.v14)
     ],
     products: [
         .library(
             name: "ReverseExtension",
             targets: ["ReverseExtension"]
-        ),
+        )
     ],
-    dependencies: [], 
+    dependencies: [],
     targets: [
-        // Your main library target
         .target(
             name: "ReverseExtension",
             dependencies: [],
             path: "ReverseExtension",
-            exclude: [    // 👇 ignore Obj-C files
+            exclude: [
                 "DelegateProxyBase.h",
                 "DelegateProxyBase.m",
                 "DenyDelegateMethod.h",
                 "DenyDelegateMethod.m"
             ]
-        ),
+        )
     ]
 )
